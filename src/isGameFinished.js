aaecdbb336e24dde412c0ac0a3a1d621aa9173d1
Philipp Kursawe <phil.kursawe@gmail.com>
2019-10-17 09:18:59 +0200

    fix: Conside games where no 1 holds a hand also finished

--- src/isGameFinished.js
+++ src/isGameFinished.js
@@ -1,11 +1,12 @@
 /**
  * A game is finished when none but one deck has cards left
  *
- * @param {Array} decks of cards
+ * @param {Array} decks Array of cards
  *
  * @return true if the game is finished
  */
 function isGameFinished(decks) {
-  return decks.filter(deck => deck.length).length === 1
+  const decksWithCards = decks.filter(deck => deck.length)
+  return decksWithCards.length <= 1
 }
 module.exports = isGameFinished
--- test/isGameFinished.test.js
+++ test/isGameFinished.test.js
@@ -3,10 +3,14 @@ const t = require("tap")
 const isGameFinished = require("../src/isGameFinished")
 
 t.test("is game finished", t => {
-  t.plan(2)
+  t.plan(3)
   t.ok(isGameFinished([
     [],
     [1,2,3,4]
+  ]))
+  t.ok(isGameFinished([
+    [],
+    []
   ]))
   t.notOk(isGameFinished([
     [1],

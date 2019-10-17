5d0a3262aee6d4ba2526672de84d82cb866c74e2
Philipp Kursawe <phil.kursawe@gmail.com>
2019-10-17 09:17:52 +0200

    fix: `pure` dealing of cards
    Better use some pure function here and return the result of the card dealings.

--- src/dealCards.js
+++ src/dealCards.js
@@ -1,13 +1,22 @@
-function dealCards(cards, players) {
-  const numberOfPlayers = players.length
-  players.forEach(player => player.cards = [])
-  
+/**
+ * Deals from a deck of cards to a number of players.
+ * 
+ * @param {Array} cards to deal from
+ * @param {Number} numberOfPlayers to deal the cards to
+ * 
+ * @returns {Array} of player hands
+ */
+function dealCards(cards, numberOfPlayers) {
+  const players = [...Array(numberOfPlayers)].map(() => {
+    return []
+  })
+
   var playerIndex = 0
   for (var card of cards) {
-    const player = players[playerIndex]
-    player.cards.push(card)
+    players[playerIndex].push(card)
     playerIndex = (playerIndex + 1) % numberOfPlayers
   }
+  return players
 }
 
 module.exports = dealCards
